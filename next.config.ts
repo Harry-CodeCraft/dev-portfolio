26fdf521f90dd36375d2bb17074f7f56d7a1f2af
Harry Sin <harsh10352@gmail.com>
2025-06-10 19:41:10 +0530

    added config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    siteName: "HarryCodeCraft",
+    siteUrl: "https://www.harrycodecraft.com",
+    authorName: "Harsh Singh",
+    authorUrl: "https://www.harrycodecraft.com",
+    enableComingSoon: process.env.ENABLE_COMING_SOON === "true",
+  },
 };
 
 export default nextConfig;
