9075302ade155bcc44a51718776fba0e42f6d96d
Harry Sin <harsh10352@gmail.com>
2025-06-15 16:57:20 +0530

    added config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,40 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://**/**")],
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "i.vimeocdn.com",
+      "www.creativefabrica.com",
+      "image.winudf.com",
+      "www.leadsquared.com",
+    ], // Add all the hostnames from your image URLs
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        pathname: "**", // Match any path
+      },
+      {
+        protocol: "https",
+        hostname: "i.vimeocdn.com",
+        pathname: "**", // Match any path
+      },
+      {
+        protocol: "https",
+        hostname: "www.creativefabrica.com",
+        pathname: "**", // Match any path
+      },
+      {
+        protocol: "https",
+        hostname: "image.winudf.com",
+        pathname: "**", // Match any path
+      },
+      {
+        protocol: "https",
+        hostname: "www.leadsquared.com",
+        pathname: "**", // Match any path
+      },
+    ],
   },
   publicRuntimeConfig: {
     siteName: "HarryCodeCraft",
